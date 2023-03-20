48fc2ebeae25d4cf463d96f8aba612d879c13353
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-20 10:25:23 +0800

    Create HW2_4105031027.java

---
+++ HW/HW2/HW2_4105031027.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class HW2_4105031027 {
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		int a;
+		int b;
+		
+		System.out.println("Enter numbers a <= b:");
+		a=scn.nextInt();
+		b=scn.nextInt();
+	}
+}
