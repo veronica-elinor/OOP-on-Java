ee60eeff1e6b6ea36d516858d1b79c1c5ad3cdde
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-20 12:03:34 +0800

    Update HW2_4105031027.java

--- HW/HW2/HW2_4105031027.java
+++ HW/HW2/HW2_4105031027.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class HW2_4105031027 {
 	public static void main(String[] args) {
 		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter numbers a <= b:");
 		// input setting
-		int a;
-		int b;
-		
+		a=scn.nextInt();
+		b=scn.nextInt();
+
 		// Check validity
 		if (a <= 0 || b <= 0 || b < a || a > 10000 || b > 10000) {
 			System.out.println("Invalid input");
@@ -35,7 +36,7 @@ public class Main {
         int b = scanner.nextInt();
 
         // Check input validity
-        if (a <= 0 || b <= 0 || b < a || a > 10000 || b > 10000) {
+        if (a<=0||b<= 0||b<a||a>10000||b>10000) {
             System.out.println("Invalid input");
             return;
         }
