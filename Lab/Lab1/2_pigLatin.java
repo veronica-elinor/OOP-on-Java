81104e71401fd84fc2363e5fb52d7aeb1b1cea96
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-20 10:13:01 +0800

    Create 2_pigLatin.java
    still not fininsh

---
+++ Lab/Lab1/2_pigLatin.java
@@ -0,0 +1,5 @@
+public static void main(String[] args){
+  first=first.substring(1,2).toUpperCase()+first.substring(3)+first.substring(0,1)+"ay";
+      last=last.substring(1,2).toUpperCase()+last.substring(3)+last.substring(0,1)+"ay";
+      return;
+}
