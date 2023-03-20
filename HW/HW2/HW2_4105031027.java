4a7a002ce06835bd1b6d30119cc89f67d7f8ba67
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-20 11:18:51 +0800

    Update HW2_4105031027.java
    The main method reads the input values, checks their validity, and iterates through all numbers between a and b. For each number, it calls the hasRepeatedDigits function to check if it contains repeated digits. If it does, the number is skipped. If it doesn't, the count variable is incremented.
    
    The hasRepeatedDigits function checks if a number contains repeated digits by calling the countDigitOccurrence function for each digit from 0 to 9. If the count is greater than 1, the number contains repeated digits and the function returns true. Otherwise, it returns false.
    
    The countDigitOccurrence function counts how many times a given digit occurs in a number by iterating through its digits and counting the occurrences.
    
    To test the program, you can run it and input two integers a and b as specified in the prompt. The program will output the number of valid house street numbers between a and b. If the input is not valid, the program will output "Invalid input".

--- HW/HW2/HW2_4105031027.java
+++ HW/HW2/HW2_4105031027.java
@@ -13,3 +13,57 @@ public class HW2_4105031027 {
 	public static int countDigitOccurrence (int n, int digit){
 	}
 }
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read input
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        // Check input validity
+        if (a <= 0 || b <= 0 || b < a || a > 10000 || b > 10000) {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        int count = 0;
+
+        // Iterate through all numbers between a and b
+        for (int i = a; i <= b; i++) {
+            if (hasRepeatedDigits(i)) {
+                continue;  // skip numbers with repeated digits
+            }
+            count++;
+        }
+
+        System.out.println(count);
+    }
+
+    // Returns true if n contains repeated digits, false otherwise
+    public static boolean hasRepeatedDigits(int n) {
+        for (int i = 0; i <= 9; i++) {
+            if (countDigitOccurrence(n, i) > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Returns how many times the digit occurs in the number n
+    public static int countDigitOccurrence(int n, int digit) {
+        int count = 0;
+        while (n > 0) {
+            if (n % 10 == digit) {
+                count++;
+            }
+            n /= 10;
+        }
+        return count;
+    }
+
+}
