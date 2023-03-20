5d06c763503d73e852907506812a9a06140f2371
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-20 10:17:54 +0800

    Create 2_factorial.java
    still not finished

---
+++ Lab/Lab3/2_factorial.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+	Scanner scn=new Scanner(System.in);
+    int n;
+    int factorial=1;
+    n=scn.nextInt();
+    
+    	for(int factorial=1; n>0 && n<=12; n--)
+    	{
+        fratorial*=n;
+    	}
+    System.out.println(fratorial);	
+    }
