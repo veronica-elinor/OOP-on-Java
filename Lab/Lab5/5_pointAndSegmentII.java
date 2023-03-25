803442a19e61746ad39dba769b630564491fe6a3
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-25 11:16:15 +0800

    Create 5_pointAndSegmentII.java

---
+++ Lab/Lab5/5_pointAndSegmentII.java
@@ -0,0 +1,23 @@
+class Point {
+    // Same as previous question
+}
+
+class Line {
+    // Declaring member variables here
+
+    public Line(Point p1, Point p2) {
+        // please write your code here
+    }
+
+    public Line(Point p1) {
+        // please write your code here
+    }
+
+    public double getLength() {
+        // please write your code here
+    }
+
+    public Point getIntersection(Line l) {
+        // please write your code here
+    }
+}
