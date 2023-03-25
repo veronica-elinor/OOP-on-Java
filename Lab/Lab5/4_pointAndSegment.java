fb563cc07ad7682ce1a653addf06b6009c4968ab
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-25 11:14:35 +0800

    Create 4_pointAndSegment.java

---
+++ Lab/Lab5/4_pointAndSegment.java
@@ -0,0 +1,22 @@
+class Point {
+    // Declaring member variables here
+    double x;
+    double y;
+
+    public Point(double x, double y) {
+        // please write your code here
+      	setPoint(x, y);
+    }
+
+    public double getX() {
+        // please write your code here
+    }
+
+    public double getY() {
+        // please write your code here
+    }
+
+    public double getDistance(Point p) {
+        // please write your code here
+    }
+}
