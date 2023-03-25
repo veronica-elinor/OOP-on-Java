d622350e95cd5a66d908c976f26ca38f474fe004
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-25 10:01:57 +0800

    Update HW2_4105031027.java
    still not finished

--- HW/HW2/HW2_4105031027.java
+++ HW/HW2/HW2_4105031027.java
@@ -4,22 +4,29 @@ public class HW2_4105031027 {
 	public static void main(String[] args) {
 		Scanner scn=new Scanner(System.in);
 		System.out.println("Enter numbers a <= b:");
-		// input setting
-		a=scn.nextInt();
-		b=scn.nextInt();
+		
+		// declare
+		int a=scn.nextInt();
+		int b=scn.nextInt();
 
 		// Check validity
-		if (a <= 0 || b <= 0 || b < a || a > 10000 || b > 10000) {
+		if (a<=0 || b<=0 || b<a || a>10000 || b>10000) {
 			System.out.println("Invalid input");
 			return;
 		}
 		
-		System.out.println("Enter numbers a <= b:");
-		a=scn.nextInt();
-		b=scn.nextInt();
 	}
 	
-	public static int countDigitOccurrence (int n, int digit){
+	// Returns how many times the digit occurs in the number n
+	public static int countDigitOccurrence(int n, int digit) {
+		int count = 0;
+		while (n > 0) {
+			if (n % 10 == digit) {
+				count++;
+			}
+			n /= 10;
+		}
+		return count;
 	}
 }
 
