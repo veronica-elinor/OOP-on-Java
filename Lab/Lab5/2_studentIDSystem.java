9932d30b16e953d399ffa2149b768feedef42cbf
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-25 11:05:29 +0800

    Create 2_studentIDSystem.java
    not finished

---
+++ Lab/Lab5/2_studentIDSystem.java
@@ -0,0 +1,31 @@
+class Student {
+  // Declaring member variables here
+  int id;
+  String name;
+  String department;
+  
+  public Student() {
+    // please write your code here
+  }
+
+  public Student(int id) {
+    // please write your code here
+    this.id=id;
+  }
+
+  public Student(int id, String name) {
+    // please write your code here
+    this.
+  }
+
+  public Student(int id, String name, String department) {
+    // please write your code here
+  }
+
+  public void getInfo() {
+    // please write your code here
+    System.out.println("Student id = "+id)
+    System.out.println("Student name = "+name)
+    System.out.println("Student department = "+department)
+  }
+}
