7d7815c4b518cd561c9f941e1fd926d4b6d60ce5
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-25 10:46:14 +0800

    Create 1_rpgGame.java
    not fininshed

---
+++ Lab/Lab5/1_rpgGame.java
@@ -0,0 +1,54 @@
+class Warrior {
+	// Declaring member variables here
+
+	public int getHP() {
+		// please write your code here
+	}
+
+	public int getMP() {
+		// please write your code here
+	}
+
+	public Warrior() {
+		// please write your code here
+	}
+
+	public Warrior(int HP, int MP) {
+		// please write your code here
+	}
+
+	public void Slash(Wizard wizard){
+		// please write your code here
+	}
+
+	public boolean CheckAlive() {
+		// please write your code here
+	}
+}
+
+class Wizard {
+	// Declaring member variables here
+	public int getHP() {
+		// please write your code here
+	}
+
+	public int getMP() {
+		// please write your code here
+	}
+
+	public Wizard() {
+		// please write your code here
+	}
+
+	public Wizard(int HP, int MP) {
+		// please write your code here
+	}
+
+	public void FireBall(Warrior warrior){
+		// please write your code here
+	}
+
+	public boolean CheckAlive() {
+		// please write your code here
+	}
+}
