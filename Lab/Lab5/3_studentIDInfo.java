290b403d6f73d316f8f5c3f3536868de73fce3cc
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-25 11:07:40 +0800

    Create 3_studentIDInfo.java

---
+++ Lab/Lab5/3_studentIDInfo.java
@@ -0,0 +1,39 @@
+class Student {
+	// Declaring member variables here
+
+	public Student() {
+		// please write your code here
+	}
+
+	public Student(int id) {
+		// please write your code here
+	}
+
+	public Student(int id, String name) {
+		// please write your code here
+	}
+
+	public Student(int id, String name, String department) {
+		// please write your code here
+	}
+
+	public void getInfo() {
+		// please write your code here
+	}
+}
+
+class StudentInfoSystem {
+	// Declaring member variables here
+
+	public StudentInfoSystem() {
+		// please write your code here
+	}
+
+	public void updateStudent(Student student) {
+		// please write your code here
+	}
+
+	public void printInfo() {
+		// please write your code here
+	}
+}
