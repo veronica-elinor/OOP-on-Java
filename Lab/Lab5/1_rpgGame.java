67eb459dad7a61f42df3264590c87b6f69e408b6
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-27 00:22:05 +0800

    Update 1_rpgGame.java

--- Lab/Lab5/1_rpgGame.java
+++ Lab/Lab5/1_rpgGame.java
@@ -104,3 +104,111 @@ public class Main {
 		}
 	}
 }
+
+//chatGPT
+class Warrior {
+    private int HP;
+    private int MP;
+    
+    public Warrior() {
+        this.HP = 700;
+        this.MP = 100;
+    }
+    
+    public Warrior(int HP, int MP) {
+        this.HP = HP;
+        this.MP = MP;
+    }
+    
+    public int getHP() {
+        return this.HP;
+    }
+    
+    public int getMP() {
+        return this.MP;
+    }
+    
+    public void Slash(Wizard wizard) {
+        if (this.MP >= 20) {
+            this.MP -= 20;
+            wizard.takeDamage(60);
+        }
+    }
+    
+    public boolean CheckAlive() {
+        if (this.HP <= 0) {
+            System.out.println("Wizard win");
+            return false;
+        }
+        return true;
+    }
+    
+    public void takeDamage(int damage) {
+        this.HP -= damage;
+    }
+}
+
+class Wizard {
+    private int HP;
+    private int MP;
+    
+    public Wizard() {
+        this.HP = 300;
+        this.MP = 500;
+    }
+    
+    public Wizard(int HP, int MP) {
+        this.HP = HP;
+        this.MP = MP;
+    }
+    
+    public int getHP() {
+        return this.HP;
+    }
+    
+    public int getMP() {
+        return this.MP;
+    }
+    
+    public void FireBall(Warrior warrior) {
+        if (this.MP >= 100) {
+            this.MP -= 100;
+            warrior.takeDamage(140);
+        }
+    }
+    
+    public boolean CheckAlive() {
+        if (this.HP <= 0) {
+            System.out.println("Warrior win");
+            return false;
+        }
+        return true;
+    }
+    
+    public void takeDamage(int damage) {
+        this.HP -= damage;
+    }
+}
+
+public class RPGGame {
+    public static void main(String[] args) {
+        Warrior warrior = new Warrior();
+        Wizard wizard = new Wizard();
+        
+        while (true) {
+            warrior.Slash(wizard);
+            if (!wizard.CheckAlive()) {
+                break;
+            }
+            wizard.FireBall(warrior);
+            if (!warrior.CheckAlive()) {
+                break;
+            }
+            warrior.Slash(wizard);
+            if (!wizard.CheckAlive()) {
+                break;
+            }
+        }
+    }
+}
+
