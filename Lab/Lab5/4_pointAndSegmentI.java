7ef2dcfd0e4486b34a0e31d1e1d39637fb6619e4
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-27 17:47:07 +0800

    Update 4_pointAndSegmentI.java

--- Lab/Lab5/4_pointAndSegmentI.java
+++ Lab/Lab5/4_pointAndSegmentI.java
@@ -1,22 +1,28 @@
 class Point {
     // Declaring member variables here
-    double x;
-    double y;
+	private double x;
+    private double y;
 
     public Point(double x, double y) {
         // please write your code here
-      	setPoint(x, y);
+        this.x = x;
+		this.y = y;
     }
 
     public double getX() {
         // please write your code here
+        return x;
     }
 
     public double getY() {
         // please write your code here
+        return y;
     }
 
     public double getDistance(Point p) {
         // please write your code here
+        double dx = x - p.x;
+        double dy = y - p.y;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 }
