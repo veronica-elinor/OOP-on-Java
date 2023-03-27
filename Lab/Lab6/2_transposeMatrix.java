af735aa5fc52bc83e8fdc308c7283ca18cda4b60
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-27 23:13:21 +0800

    Update 2_transposeMatrix.java

--- Lab/Lab6/2_transposeMatrix.java
+++ Lab/Lab6/2_transposeMatrix.java
@@ -1,31 +1,37 @@
 public class Main {
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-        int m = scn.nextInt();
-        int n = scn.nextInt();
-    	Ststem.out.println(m,n);
+	        int m = scn.nextInt();
+	        int n = scn.nextInt();
 
-        int[][] matrix = new int[m][n];
-    	for (int i=0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = scn.nextInt();
-            }
-        }
+	        int[][] matrix = new int[m][n];
+	        for (int i = 0; i < m; i++) {
+	            for (int j = 0; j < n; j++) {
+	                matrix[i][j] = scn.nextInt();
+	            }
+	        }
+	        
+	        scn.close();
 
-        int[][] transpose = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                transpose[i][j] = matrix[j][i];
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(transpose[i][j]);
-              	if (j=m) {System.out.print("\n");}
-              	else {System.out.print(" ");}
-            }
-            System.out.println();
-        }
+	        int[][] transpose = new int[n][m];
+	        for (int i = 0; i < n; i++) {
+	            for (int j = 0; j < m; j++) {
+	                transpose[i][j] = matrix[j][i];
+	            }
+	        }
+	    	
+	    	System.out.println(m+" "+n);
+	    	
+	        for (int i = 0; i < n; i++) {
+	            for (int j = 0; j < m; j++) {
+	                System.out.print(transpose[i][j]);
+	              if (j == m-1) {
+	                System.out.print("\n");
+	              }
+	              else {
+	                System.out.print(" ");
+	              }
+	            }
+	        }
   }
 }
