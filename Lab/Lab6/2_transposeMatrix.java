5a1e8caab97787abfc44abffb29746efad986bd8
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-27 11:33:31 +0800

    Create 2_transposeMatrix.java

---
+++ Lab/Lab6/2_transposeMatrix.java
@@ -0,0 +1,29 @@
+public class Main {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scn.nextInt();
+            }
+        }
+
+        int[][] transpose = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                transpose[i][j] = matrix[j][i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(transpose[i][j]);
+              	if (
+            }
+            System.out.println();
+        }
+  }
+}
