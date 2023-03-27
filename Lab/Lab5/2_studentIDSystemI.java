1a7cf57d2380a61ad0c36577446826af3b9aac16
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-03-27 16:40:18 +0800

    Update 2_studentIDSystemI.java
    finished

--- Lab/Lab5/2_studentIDSystemI.java
+++ Lab/Lab5/2_studentIDSystemI.java
@@ -1,31 +1,53 @@
 class Student {
   // Declaring member variables here
-  int id;
-  String name;
-  String department;
-  
+  private int id;
+  private String name;
+  private String department;
+
   public Student() {
     // please write your code here
+    id = 0;
+    name = "John";
+    department = "Computer Science";
   }
 
   public Student(int id) {
     // please write your code here
-    this.id=id;
+    this.id = id;
   }
 
   public Student(int id, String name) {
     // please write your code here
-    this.
+    this.id = id;
+    this.name = name;
   }
 
   public Student(int id, String name, String department) {
     // please write your code here
+    this.id = id;
+    this.name = name;
+    this.department = department;
   }
 
   public void getInfo() {
     // please write your code here
-    System.out.println("Student id = "+id)
-    System.out.println("Student name = "+name)
-    System.out.println("Student department = "+department)
+    System.out.println("Student id = " + id);
+    System.out.println("Student name = " + name);
+    System.out.println("Student department = " + department);
   }
+}
+
+//testing function
+public class Test {
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		Student student2 = new Student(1);
+		Student student3 = new Student(2, "Bob");
+		Student student4 = new Student(3, "Alice", "Electrical Engineering");
+
+		student1.getInfo();
+		student2.getInfo();
+		student3.getInfo();
+		student4.getInfo();
+	}
 }
