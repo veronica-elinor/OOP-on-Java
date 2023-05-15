549d7518ec05231e4389913f7375ec33db02f77a
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-05-15 23:11:39 +0800

    Update Lab10_Q3_4105031027.java

--- Lab/Lab10/Q3/Lab10_Q3_4105031027.java
+++ Lab/Lab10/Q3/Lab10_Q3_4105031027.java
@@ -1,16 +1,38 @@
-class StudentDataIO{
-  public void writeData(Student[] students, String filename){}
-  public Student[] readData(String filename)
-}
 class StudentDataIO {
     public void writeData(Student[] students, String filename) {
         // TODO: Write each student's information to the file.
+	    try (PrintWriter writer = new PrintWriter(filename)) {
+            for (Student student : students) {
+                writer.println(student.getId() + "," + student.getName() + "," + student.getScore());
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing the data to the file.");
+            e.printStackTrace();
+        }
     }
 
     public Student[] readData(String filename) {
         // TODO: Read the student information from the file and return a list containing
         // all the students.
-	    
+	    try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length == 3) {
+                    String id = data[0];
+                    String name = data[1];
+                    int score = Integer.parseInt(data[2]);
+                    Student student = new Student(id, name, score);
+                    studentList.add(student);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("The file was not found.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the data from the file.");
+        }
+
+        return studentList.toArray(new Student[0]);
     }
 }
 
