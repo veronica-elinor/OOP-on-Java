a4c1981173bdceb27e18fb1eb5de1870b9097f21
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-05-15 12:01:14 +0800

    Update Lab10_Q3_4105031027.java

--- Lab/Lab10/Q3/Lab10_Q3_4105031027.java
+++ Lab/Lab10/Q3/Lab10_Q3_4105031027.java
@@ -1,16 +1,3 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-public class Lab10_Q3_4105031027 {
-	public static void main(String[] args) {
-		File filename = new File("input.txt");
-		
-		System.out.println("The number of occurrences of number 10 is "+count);
-	}
-}
-
 class StudentDataIO{
   public void writeData(Student[] students, String filename){}
   public Student[] readData(String filename)
@@ -23,6 +10,7 @@ class StudentDataIO {
     public Student[] readData(String filename) {
         // TODO: Read the student information from the file and return a list containing
         // all the students.
+	    
     }
 }
 
