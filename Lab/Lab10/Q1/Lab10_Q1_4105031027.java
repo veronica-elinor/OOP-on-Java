946b4325d227edd1bb6792f7e153225439454433
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-05-15 11:27:04 +0800

    Create Lab10_Q1_4105031027.java

---
+++ Lab/Lab10/Q1/Lab10_Q1_4105031027.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Lab10_Q1_4105031027 {
+	public static void main(String[] args) {
+		File filename = new File("input.txt");
+		
+		System.out.println("The number of occurrences of number 10 is "+count);
+	}
+}
