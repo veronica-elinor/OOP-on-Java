f0fe4f9cc50ecec519041b28faa062f1d943aaef
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-05-15 22:58:31 +0800

    Update Lab10_Q1_4105031027.java

--- Lab/Lab10/Q1/Lab10_Q1_4105031027.java
+++ Lab/Lab10/Q1/Lab10_Q1_4105031027.java
@@ -5,8 +5,31 @@ import java.util.Scanner;
 
 public class Lab10_Q1_4105031027 {
 	public static void main(String[] args) {
-		File filename = new File("input.txt");
+		Scanner inputStream = null;
 		
+		try {
+			inputStream = new Scanner (new FileInputStream("workspace/input.txt"));
+            		
+            
+            	}
+		catch (FileNotFoundException e) {
+            		System.out.println("File input.txt was not found.");
+			System.out.println("or could not be opened.");
+			System.exit(0);
+		}
+		
+		int num;
+            	int count = 0;
+            
+            	while ((num = inputStream.nextInt()) != null) {
+			String[] numbers = num.split(" ");
+			for (String number : numbers) {
+				if (number.equals("10")) {
+					count++;
+				}
+			}
+		}
 		System.out.println("The number of occurrences of number 10 is "+count);
+		inputStream.close();
 	}
 }
