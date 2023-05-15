cd34472900148dc340af13da4738863c713e6fc5
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-05-15 23:12:11 +0800

    Update Lab10_Q2_4105031027.java

--- Lab/Lab10/Q2/Lab10_Q2_4105031027.java
+++ Lab/Lab10/Q2/Lab10_Q2_4105031027.java
@@ -5,8 +5,23 @@ import java.util.Scanner;
 
 public class Lab10_Q2_4105031027 {
 	public static void main(String[] args) {
-		File filename = new File("input.txt");
-		
+		File filename = new File("workspace/input.txt");
+		try {
+            		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		}
+		catch (IOException e) {
+			System.out.println("An error occurred while reading the file.");
+		}
+		String num;
+		double sum = 0;
+		int count = 0;
+		while ((num = reader.readLine()) != null) {
+                	double number = Double.parseDouble(num);
+                	sum += number;
+                	count++;
+		}
+		double average = sum / count;
 		System.out.println("Average: "+average);
+		reader.close();
 	}
 }
