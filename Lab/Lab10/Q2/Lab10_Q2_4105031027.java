dd9a572d7fbd4216024be8dd175546dcd3808391
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-05-15 11:30:01 +0800

    Create Lab10_Q2_4105031027.java

---
+++ Lab/Lab10/Q2/Lab10_Q2_4105031027.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Lab10_Q2_4105031027 {
+	public static void main(String[] args) {
+		File filename = new File("input.txt");
+		
+		System.out.println("Average: "+average);
+	}
+}
