e6a8e75d44b61da64f8fde15940fecff7717aea2
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-06-05 11:24:41 +0800

    Create 1_thread.java

---
+++ Lab/Lab13/1_thread.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class PrintThread implements Runnable {
+	// write your code here
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int intervalT1 = sc.nextInt();
+        int intervalT2 = sc.nextInt();
+		// write your code here
+    }
+}
