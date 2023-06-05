19de3007ce5e65c79bac8923da4e01207537653b
Veronica Eleanor <66841183+veronica-elinor@users.noreply.github.com>
2023-06-05 23:11:42 +0800

    Update 2_studentManager.java

--- Lab/Lab13/2_studentManager.java
+++ Lab/Lab13/2_studentManager.java
@@ -1,11 +1,12 @@
 class Student {
     // write your code here
   private String name;
-  private int id;
+    private int id;
+  
     public Student(String name, int id) {
         // write your code here
-      this.name=name;
-      this.id=id;
+      this.name = name;
+        this.id = id;
     }
     
     public String getName() {
@@ -21,22 +22,34 @@ class Student {
 
 class StudentListManager {
 	// write your code here
-	LinkedList<Student> Students=new LinkedList<Student>();
+  private LinkedList<Student> studentList;
   
+  public StudentListManager() {
+        studentList = new LinkedList<>();
+    }
 
     public void addStudent(String name, int id) {
         // write your code here
-      
+      Student student = new Student(name, id);
+        studentList.add(student);
     }
     
     public void removeStudent(int id) {
         // write your code here
+      ListIterator<Student> iterator = studentList.listIterator();
+        while (iterator.hasNext()) {
+            Student student = iterator.next();
+            if (student.getId() == id) {
+                iterator.remove();
+                break;
+            }
+        }
     }
     
     public void printStudentList() {
         // write your code here
-      for(int i:arr){
-            System.out.print("Name:"+name",ID:"+id);
+      for (Student student : studentList) {
+            System.out.println("Name:"+student.getName()+",ID:"+student.getId());
         }
     }
 }
